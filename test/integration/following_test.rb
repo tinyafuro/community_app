ad3c6050eb7115809bc43d02c20a00dc2d01b9f0
tinyafuro <tinyforest.win@gmail.com>
2019-02-08 22:22:29 +0900

    Add user following2

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -60,4 +60,13 @@ class FollowingTest < ActionDispatch::IntegrationTest
     end
   end
 
+  # Homeページで表示される１ページ目のフィードに対するテスト
+  test "feed on Home page" do
+    get root_path
+    @user.feed.paginate(page: 1).each do |micropost|
+      assert_match CGI.escapeHTML(micropost.content), response.body
+      # assert_match micropost.content, response.body
+    end
+  end
+
 end
